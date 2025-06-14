58ef3ef2845b4b0c4c003a52287664123eb177ee
Callum Preston <CallumCPreston@outlook.com>
2025-06-14 18:56:23 +1000

    Create basic UI, form, and form handling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 import "./styles.css";
+import { todoItem } from "./new-todo";
+
+let todoList = [];
+
+const newTodo = document.querySelector("#modalOpen");
+const dialog = document.querySelector("#addTodoDialog");
+newTodo.addEventListener("click", () => {
+    dialog.showModal();
+})
+
+const submit = document.querySelector("#submitButton");
+submit.addEventListener("click", (e) => {
+    const form = document.querySelector("#addTodoForm");
+    if (!form.checkValidity()) {
+        alert("Please fill out all required fields.")
+        e.preventDefault();
+    }
+    else {
+        let todo = new todoItem(document.querySelector("#title").value, document.querySelector("#desc").value, document.querySelector("#dueDate").value, document.querySelector('input[name="todoPriority"]:checked').value, false);
+        console.log(todo);
+        todoList.push(todo);
+        dialog.close();
+        e.preventDefault();
+    }
+})
+
+const close = document.querySelector("#modalClose");
+close.addEventListener("click", (e) => {
+    dialog.close();
+    e.preventDefault();
+})
---
+++ src/new-todo.js
@@ -0,0 +1,12 @@
+class todoItem{
+    constructor(title, desc, due, prio, checklist) {
+        this.title = title;
+        this.desc = desc;
+        this.due = due;
+        this.prio = prio;
+        this.checklist = checklist;
+    }
+}
+
+export { todoItem }
+
